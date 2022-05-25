3da9d16aff539aad8d87fae3c8f391df8c7a2542
SahilKumarGit <email.sahil@yahoo.com>
2022-05-25 18:43:46 +0530

    user update all bug fived

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 let router = express.Router();
 const controller = require('../controllers/userController')
+const { authentication, authorization_user} = require('../middleware/auth')
 
 router.post('/register', controller.createUser)
 router.post('/login', controller.login)
-router.put('/user/:userId/profile', controller.update)
-router.get('/user/:userId/profile', controller.getUser)
+router.put('/user/:userId/profile', authentication, authorization_user, controller.update)
+router.get('/user/:userId/profile', authentication, controller.getUser)
 
 module.exports = router;
