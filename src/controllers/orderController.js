5d61159269cfd8ecbc54d9caf707d45b7811e60e
SahilKumarGit <email.sahil@yahoo.com>
2022-06-01 11:47:41 +0530

    order-> creare bug fix

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,32 +1,46 @@
-const mongoose = require('mongoose')
-const orderModel = require('../model/orderModel')
+const orderModel = require('../models/orderModel')
+const chartModel = require('../models/cartModels')
 const vfy = require('../utility/validation')
 
 const createOrder = async function (req, res) {
 
-    try {let requestBody = req.body;
-        const userIdInParams = req.params.userId
+    try {
+        let requestBody = req.body;
+        const userId = req.params.userId
 
-        //  Authroization is being checked through Auth(Middleware)
-        
-        const { userId, totalPrice, totalItems, items, totalQuantity } = requestBody
+        // 👍 Authroization is being checked through Auth(Middleware)
 
-        if (vfy.isEmptyObject(requestBody)){return res.status(400).send({ status: false, Message: 'Please provide cart details' });}
+        const { cartId } = requestBody
 
-        if (!vfy.isValidObjectId(userId)){return res.status(400).send({ status:false, Message: 'userid is required in the request body' })}
+        if (vfy.isEmptyObject(requestBody)) { return res.status(400).send({ status: false, Message: '☹️ Please provide Post Body' }); }
 
-        if (vfy.isEmptyVar(userId)){return res.status(400).send({ status: false, Message: `${userId} is not a valid user id` })}
+        if (vfy.isEmptyVar(cartId)) { return res.status(400).send({ status: false, Message: '☹️ Please provide cartId' }) }
+        if (!vfy.isValidObjectId(cartId)) { return res.status(400).send({ status: false, Message: '☹️ Please provide a valid cartId' }) }
 
+        // use userid to find cart
+        const cart = await chartModel.findOne({ userId })
+        if (!cart) return res.status(404).send({ status: false, Message: '☹️ user\'s cart unavailable' })
+        if (cart._id != cartId) return res.status(400).send({ status: false, Message: '☹️ Cart id doesn\'t belong to this user' })
 
+        // get cart info like items, totalPrice, totalItems and totalQuantity
+        let { items, totalPrice, totalItems } = cart
+        let totalQuantity = 0;
+        items.forEach(each => totalQuantity += each.quantity);
 
+        // object that use to create order
+        const Obj = {
+            userId,
+            items,
+            totalPrice,
+            totalItems,
+            totalQuantity
+        }
 
+        const createProduct = await orderModel.create(Obj);
 
+        res.status(201).send({ status: true, Message: '✅ sucesfully created order', data: createProduct })
 
-        const createProduct = await orderModel.create(requestBody);
-
-        res.status(200).send({ status: true, Message: 'sucesfully created order', data: createProduct })
-
-    } catch (error){res.status(500).send({ status: false, Message: error.message })}
+    } catch (error) { res.status(500).send({ status: false, Message: error.message }) }
 }
 
-module.exports = {createOrder}
+module.exports = { createOrder }
--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -2,23 +2,23 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
 
-userId:{type: mongoose.Schema.Types.ObjectId,ref: 'User',required: true},
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 
-items: [{productId: {type: mongoose.Schema.Types.ObjectId,ref: 'Product',required: true},quantity: {type: Number,required: true,min: 1}}],
+    items: [{ productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, quantity: { type: Number, required: true, min: 1 } }],
 
-totalPrice: {type: Number,required: true},
+    totalPrice: { type: Number, required: true },
 
-totalItems: {type: Number,required: true},
+    totalItems: { type: Number, required: true },
 
-totalQuantity: {type: Number,required: true},
+    totalQuantity: { type: Number, required: true },
 
-cancellable: {type: Boolean,default: true},
+    cancellable: { type: Boolean, default: true },
 
-status: {type: String,default: 'pending',enum: ["pending", "completed", "canceled"]},
+    status: { type: String, default: 'pending', enum: ["pending", "completed", "canceled"] },
 
-deletedAt: {type: Date,default: null},
+    deletedAt: { type: Date, default: null },
 
-isDeleted: {type: Boolean,default: false},
+    isDeleted: { type: Boolean, default: false },
 
 }, { timestamps: true });
 
